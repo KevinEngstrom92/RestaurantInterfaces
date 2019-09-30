5f6a336e77fd4268eeb9b0d7358bea0af0963aa7
Kevin Engström <kevin.engstrom@hotmail.com>
2019-09-30 08:48:32 +0200

    Add readme and true for switchkey

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace RestaurantInterfaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Menu in De_restaurant:")
+            Console.WriteLine("Menu in De_restaurant:");
+
+            Console.WriteLine("1.\n2.\n3.\n4.\n");
+
+            ConsoleKeyInfo input = Console.ReadKey(true);
+
+            switch (input.Key)
+            {
+                case ConsoleKey.D1:
+                    break;
+                case ConsoleKey.D2:
+                    break;
+                case ConsoleKey.D3:
+                    break;
+                case ConsoleKey.D4:
+                    break;
+
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+# Restaurant Interfaces Excersise
+
+Simple terminalbased application showcasing interfaces.
+By recieving and process various orders for delicious food.
+
+## Getting started
+
+Just clone the repo, restore the NuGet packages and you're good to go!
