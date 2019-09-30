7a469f377c83b330c85c32f2a8938179e66766ff
Kevin Engström <kevin.engstrom@hotmail.com>
2019-09-30 08:41:41 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestaurantInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
